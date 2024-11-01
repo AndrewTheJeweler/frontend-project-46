f30570293d1ea229b8e0423c1aca0ddc6d954ec2
AndrewTheJeweler <bushuevandrew96@gmail.com>
2024-11-01 21:58:22 +0300

    add comparison of yml formats

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,3 +11,8 @@ const result = stringify(JSON.parse(readFile('expected_file.json')));
 test('generateDifference', () => {
   expect(genDiff('file1.json', 'file2.json')).toEqual(result);
 });
+
+test('comparison of two formats', () => {
+  expect(genDiff('file1.json', 'file2.json') === genDiff('file1.yml', 'file2.yml')).toBeTruthy();
+  expect(genDiff('file1.json', 'file1.yml') === genDiff('file1.yml', 'file2.json')).not.toBeTruthy();
+});
